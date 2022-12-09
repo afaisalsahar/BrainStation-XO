de780be7b3aa0f356f04545c198c3c68b72fd330
afaisalsahar <afaisalsahar@gmail.com>
2022-12-08 21:08:48 -0500

    Feature/ user pick marker circle or time

--- client/app.js
+++ client/app.js
@@ -1,12 +1,31 @@
 let gameMode = "";
+let playerOne = "";
+let playerTwo = "";
+let activePlayer = playerOne;
+let firstPlayer = playerTwo;
 
 // choose game mode
 $(".mode__type").on("click", function(_e) {
     if (this.dataset.mode) {
         gameMode = this.dataset.mode;
     }
+
     $(".mode")
         .fadeOut(300, function() {
-            // TODO
+            $(".side")
+                .fadeIn()
+                .css("display", "flex");
     });
+});
+
+// choose marker / circle or times
+$(".side__icon").on("click", function(_e) {
+
+    if (this.dataset.side) {
+        playerOne = this.dataset.side;
+        activePlayer = playerOne;
+        firstPlayer = playerOne;
+    }
+
+    playerTwo = (playerOne === 'circle') ? 'times' : 'circle';
 });
