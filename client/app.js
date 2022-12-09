f099df10ccf7fab602e553a21e11cc997306703c
afaisalsahar <afaisalsahar@gmail.com>
2022-12-08 23:12:12 -0500

    Feature/user pick singleplayer difficulty level

--- client/app.js
+++ client/app.js
@@ -4,6 +4,8 @@ let playerTwo = "";
 let activePlayer = playerOne;
 let firstPlayer = playerTwo;
 
+let gameLevel = 0;
+
 // choose game mode
 $(".mode__type").on("click", function(_e) {
     if (this.dataset.mode) {
@@ -28,4 +30,29 @@ $(".side__icon").on("click", function(_e) {
     }
 
     playerTwo = (playerOne === 'circle') ? 'times' : 'circle';
+
+    if (gameMode === "single") {
+            $(".level")
+                .addClass(playerOne === "circle" ? "level--circle" : "level--times");
+    
+            $(playerOne === "circle" ? ".level__icon--circle" : ".level__icon--times")
+                .css("display", "block");
+        }
+    
+        $(".side")
+            .fadeOut(300, function() {
+                if (gameMode === "single") {
+                    $(".level")
+                        .fadeIn()
+                        .css("display", "flex");
+                } else {
+                    $(".game").fadeIn().css("display", "flex");
+                }
+        });
+});
+
+$(".level__size").on("click", function(e) {
+    if (!this.dataset.level) return false;
+
+    gameLevel = parseInt(this.dataset.level);
 });
